ac0c139e044010a08803e26d1bd0b8340b7fc1ed
Matias Prieto <matiashernan132@gmail.com>
2024-06-23 16:57:06 -0300

    Actualizacion PrietoMatias

--- src/components/components-in/Formulario.jsx
+++ src/components/components-in/Formulario.jsx
@@ -1,136 +1,119 @@
 import { useState, useEffect } from "react";
-const API = `http://localhost:3000/api/obtener/datos/`;
 
-const ReservForm = () =>
-    {
-        const [resForm, setResForm]= useState (true);
-        const [resDatos, setResDatos]=useState({
-            fecha:'', mesas:'', hora:'',nombre:'',apellido:'',email:'',tel:'',
-        })
-        
-        const [fechaMin,setFechaMin] = useState("");
+const ReservForm = () => {
+  const [resDatos, setResDatos] = useState({
+    fecha: '', cantidad: '', hora: '', nombre: '', apellido: '', correo: '', telefono: '',
+  });
+  const [fechaMin, setFechaMin] = useState("");
+  const [formVisible, setFormVisible] = useState(false);
 
-        const limpiarInp = () =>{
-            setResDatos({
-                fecha:'', mesas:'', hora:'',nombre:'',apellido:'',email:'',tel:'',
-            })
+  const limpiarInp = () => {
+    setResDatos({
+      fecha: '', cantidad: '', hora: '', nombre: '', apellido: '', correo: '', telefono: '',
+    });
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/insertar/reservacion`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(resDatos)
         }
+      );
+      const result = await response.json();
+      console.log('Exito:', result);
 
-        const ConfReserv = async (e) =>
-            {
-                e.preventDefault();
-                setResForm(false);
-
-                try{ 
-                const datform = await fetch(
-                    `http://localhost:3000/api/confirmar/reservacion/`,
-                    {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type' : 'application/json'},
-                    body: JSON.stringify(resDatos)    
-                });
-                const envio = await datform.json();
-                console.log('Exito:',envio);
-                }catch(error){
-                console.error('Error:',error);
-            }
-            }
-
-        const EnvDatos = async (e) =>
-            {
-                e.preventDefault();
-                
-                try{
-                     const datform = await fetch(
-                       `http://localhost:3000/api/confirmar/reservacion`,
-                      {
-                     method: 'POST',
-                     headers:{
-                        'Content-Type' : 'application/json'},
-                     body: JSON.stringify(resDatos)});
-
-                     const envio = await datform.json();
-                     console.log('Exito:',envio);
-                    } 
-                    catch(error){
-                     console.error('Error:',error);
-                    }
-            }
-        
-        const handleChange = (e) =>{
-            if(!e.target){
-                console.log("hola pajin, no valido");
-                return;
-            }
-            const {name, value} = e.target;
-            setResDatos((prev) => ({...prev, [name]: value}));
-            }
-
-            const fechas =()=>
-                {
-                    const date = new Date();
-                    const año = date.getFullYear();
-                    const mes = String(date.getMonth() + 1).padStart(2,"0");
-                    const dia = String(date.getDate()).padStart(2,"0");
-                    const fechaMin = `${año}-${mes}-${dia}`;
-                    setFechaMin(fechaMin);
-                }
-
-                useEffect(()=>{
-                    fechas();
-                    },[]);
-
-
-        return(
-            <div className="formContainer">
-                <h2 className="tituloFormulario">Reservar</h2>
-                {resForm ? (
-                    <form id="res-Form " onSubmit={ConfReserv}>
-                        <label htmlFor="fecha">Fecha:</label>
-                        <input type="date" id="fecha" name="fecha" 
-                        value={resDatos.fecha} onChange={handleChange} min={fechaMin} required/>
-
-                        <label htmlFor="personas">Mesas:</label>
-                        <input type="number" id="mesas" name="mesas" 
-                        value={resDatos.mesas} onChange={handleChange} placeholder="Cantidad de Mesas" required/>
-
-                        <label htmlFor="hora">Hora:</label>
-                        <select id="hora" name="hora" 
-                        value={resDatos.hora} onChange={handleChange} placeholder="Horario" required>
-                            <option value="19:00">19:00</option>
-                            <option value="20:00">20:00</option>
-                            <option value="21:00">21:00</option>
-                            <option value="22:00">22:00</option>
-                            <option value="23:00">23:00</option>                           
-                        </select>
-                        <button type="submit" >Reservar</button>
-                    </form>
-                
-                ):( 
-                    <form id="DatosForm" onSubmit={EnvDatos}>
-                        <h2>Detalles de la Reservacion</h2>
-                        <label htmlFor="nombre">Nombre:</label>
-                        <input type="text" id="nombre" name="nombre" 
-                        value={resDatos.nombre} onChange={handleChange} placeholder="Nombre" required/>
-
-                        <label htmlFor="apellido">Apellido:</label>
-                        <input type="text" id="apellido" name="apellido" 
-                        value={resDatos.apellido} onChange={handleChange} placeholder="Apellido" required/>
-
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email"
-                        value={resDatos.email} onChange={handleChange} placeholder="Email" required/>
-
-                        <label htmlFor="tel">Telefono:</label>
-                        <input type="number" id="tel" name="tel"
-                        value={resDatos.tel} onChange={handleChange} placeholder="Telefono" required/>
-
-                        <button type="submit" onClick={limpiarInp}>Reservar</button>
-                    </form>
-                )}
-            </div>
-        )
+      if (response.ok) {
+        limpiarInp();
+        alert('Reservación completada con éxito');
+        setFormVisible(false); // Ocultar el formulario después de enviar
+      } else {
+        console.error('Error en la respuesta:', result);
+      }
+    } catch (error) {
+      console.error('Error:', error);
     }
+  };
+
+  const handleChange = (e) => {
+    if (!e.target) {
+      console.log("Input no válido");
+      return;
+    }
+    const { name, value } = e.target;
+    setResDatos((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const fechas = () => {
+    const date = new Date();
+    const año = date.getFullYear();
+    const mes = String(date.getMonth() + 1).padStart(2, "0");
+    const dia = String(date.getDate()).padStart(2, "0");
+    const fechaMin = `${año}-${mes}-${dia}`;
+    setFechaMin(fechaMin);
+  };
+
+  useEffect(() => {
+    fechas();
+  }, []);
+
+  return (
+    <div className="formContainer">
+      <button onClick={() => setFormVisible(!formVisible)}>
+        {formVisible ? "Ocultar Formulario" : "Reservar"}
+      </button>
+
+      {formVisible && (
+        <>
+          <h2 className="tituloFormulario">Reservar</h2>
+          <form id="res-Form" onSubmit={handleSubmit}>
+            <label htmlFor="fecha">Fecha:</label>
+            <input type="date" id="fecha" name="fecha"
+              value={resDatos.fecha} onChange={handleChange} min={fechaMin} required />
+
+            <label htmlFor="mesas">Mesas:</label>
+            <input type="number" id="mesas" name="cantidad"
+              value={resDatos.cantidad} onChange={handleChange} placeholder="Cantidad de Mesas" required />
+
+            <label htmlFor="hora">Hora:</label>
+            <select id="hora" name="hora"
+              value={resDatos.hora} onChange={handleChange} required>
+              <option value="19:00">19:00</option>
+              <option value="20:00">20:00</option>
+              <option value="21:00">21:00</option>
+              <option value="22:00">22:00</option>
+              <option value="23:00">23:00</option>
+            </select>
+
+            <label htmlFor="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre"
+              value={resDatos.nombre} onChange={handleChange} placeholder="Nombre" required />
+
+            <label htmlFor="apellido">Apellido:</label>
+            <input type="text" id="apellido" name="apellido"
+              value={resDatos.apellido} onChange={handleChange} placeholder="Apellido" required />
+
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="correo"
+              value={resDatos.email} onChange={handleChange} placeholder="Email" required />
+
+            <label htmlFor="tel">Teléfono:</label>
+            <input type="number" id="tel" name="telefono"
+              value={resDatos.tel} onChange={handleChange} placeholder="Teléfono" required />
+
+            <button type="submit">Reservar</button>
+          </form>
+        </>
+      )}
+    </div>
+  );
+};
+
 export default ReservForm;
